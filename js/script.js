781e79d86e4c924c9da7129d9d0926e22004e8c1
Julia Almeida <julia.almeida.veleiros@gmail.com>
2024-09-25 23:06:36 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const toggleTheme = document.getElementById("toggleTheme");
-
 const rootHtml = document.documentElement;
 
-//função para alterar o tema
-function changeTheme (){
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+//const menuLinks = document.querySelectorAll(".menu-link");
+
+
+
+
+//função para alternar o tema 
+function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
@@ -11,7 +16,17 @@ function changeTheme (){
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 
-}
+} 
 
 toggleTheme.addEventListener("click", changeTheme);
 //fim da função mudar o tema
+
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
